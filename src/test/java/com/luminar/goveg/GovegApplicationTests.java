8a7d3474834a1d372b0909e3cd1f888ca1b8f266
vivekvinodgit <vivekvinod228@gmail.com>
2023-04-04 10:35:51 +0530

    Changes Done

---
+++ src/test/java/com/luminar/goveg/GovegApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luminar.goveg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GovegApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
